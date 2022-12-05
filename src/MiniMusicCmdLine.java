242f91b62e99f2b852a3454fc83f64f5a97689e7
danileny <evgeny_d@outlook.com>
2022-12-06 00:03:30 +0300

    minors

--- src/MiniMusicCmdLine.java
+++ src/MiniMusicCmdLine.java
@@ -9,13 +9,7 @@ public class MiniMusicCmdLine {
     public static void main(String[] args) {
         
         MiniMusicCmdLine mini = new MiniMusicCmdLine();
-        if (args.length < 2) {
-            System.out.println("Please give 2 integer arguments"); //Launch from cmd
-        } else {
-            int instrument = Integer.parseInt(args[0]);
-            int note = Integer.parseInt(args[1]);
-            mini.play(instrument, note);
-        }
+        mini.play(25, 25);
     }
     
     public void play(int instrument, int note) {
