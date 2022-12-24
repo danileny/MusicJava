316969ec17157a0accd16aafb988552f7d90b702
danileny <evgeny_d@outlook.com>
2022-12-24 18:35:14 +0300

    minors

--- src/BeatBox.java
+++ src/BeatBox.java
@@ -21,6 +21,7 @@ public class BeatBox {
     int[] instruments = {35,42,46,38,49,39,50,60,70,72,64,56,58,47,67,63};
 
 
+    
     public static void main(String[] args) {
         new BeatBox().buildGui();
     }
